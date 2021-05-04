b9f6d8ded698f26cdcc77ff0b29bba4b156d155a
Mert <mertdemirok80@gmail.com>
2021-05-04 16:29:02 +0200

    add bootstrap of publisher

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Address.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring5webapp.domain;
+
+public class Address {
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Address(String addressLine1, String city, String state, String zip) {
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressLine1='" + addressLine1 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+}
